3b16b7c6b1aaf8c50fe4fb5c71b0f8b9c4020f22
‘Brano <valasek.brano@gmail.com>
2021-09-01 21:17:38 +0200

    setting up tooling ESlint and Nodemon

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3005
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3005;
 app.get('/', (req, res) => {
-    res.send('Welcome to my app.');
+  res.send('Welcome to my Nodamon API.');
 });
 
 app.listen(port, () => {
-    console.log('Running on port ' + port);
+  console.log(`Running on port ${port}`);
 });
