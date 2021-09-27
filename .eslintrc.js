f384e24a2531ce981f179228284221d0182ba57a
‘Brano <valasek.brano@gmail.com>
2021-09-27 19:39:39 +0200

    Wiring up Mongo DB

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    'comma-dangle': 0,
   },
 };
--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+const bookRouter = express.Router();
 const port = process.env.PORT || 3005;
+const Book = require('./models/bookModel');
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    //const response = { hello: 'This is my API from express' };
+    Book.find((err, books) => {
+      if (err) {
+        return res.send(err);
+      } return res.json(books);
+    });
+  });
+
+app.use('/API', bookRouter);
+
 app.get('/', (req, res) => {
   res.send('Welcome to my Nodamon API.');
 });
