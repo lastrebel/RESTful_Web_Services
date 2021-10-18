3546222fdb05f848778598bb5cd520a611150895
‘Brano <valasek.brano@gmail.com>
2021-10-18 21:35:19 +0200

    potiing data

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
 const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
 const bookRouter = express.Router();
 const port = process.env.PORT || 3005;
 const Book = require('./models/bookModel');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 bookRouter.route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+
+    console.log(book);
+    book.save();
+    return res.status(201).json(book);
+  })
   .get((req, res) => {
     //const response = { hello: 'This is my API from express' };
     const query = {};
