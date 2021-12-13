76e6703b062e03f5693e2fdd84778ff9f1dcccdc
‘Brano <valasek.brano@gmail.com>
2021-12-13 21:21:30 +0100

    finished Testing module on Pluralsight, there are 2 environments now Prod a Test

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const bodyParser = require('body-parser');
 
 const app = express();
-const db = mongoose.connect('mongodb://localhost/bookAPI');
+
+// const db = mongoose.connect('mongodb://localhost/bookAPI-prod');
+
+if (process.env.ENV === 'Test') {
+  console.log('This is a test');
+  const db = mongoose.connect('mongodb://localhost/bookAPI_Test');
+} else {
+  console.log('This is a real');
+  const db = mongoose.connect('mongodb://localhost/bookAPI-prod');
+}
+
 const port = process.env.PORT || 3005;
 const Book = require('./models/bookModel');
 const bookRouter = require('./routes/bookRouter')(Book);
@@ -18,6 +27,8 @@ app.get('/', (req, res) => {
   res.send('Welcome to my Nodamon API.');
 });
 
-app.listen(port, () => {
+app.server = app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
+
+module.exports = app;
