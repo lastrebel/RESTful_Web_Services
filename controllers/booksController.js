b95cb627f7a54f8084035eca49c19cb99da85041
‘Brano <valasek.brano@gmail.com>
2021-12-15 21:20:48 +0100

    finish HATEOS module  but do not know how it self documents itself

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -5,8 +5,6 @@ function booksController(Book) {
       res.status(400);
       return res.send('Title is required');
     }
-
-    console.log(book);
     book.save();
     res.status(201);
     return res.json(book);
@@ -19,7 +17,14 @@ function booksController(Book) {
     Book.find(query, (err, books) => {
       if (err) {
         return res.send(err);
-      } return res.json(books);
+      }
+      const returnBooks = books.map((book) => {
+        const newBook = book.toJSON();
+        newBook.links = {};
+        newBook.links.self = `http://${req.headers.host}/api/books/${book._id}`;
+        return newBook;
+      });
+      return res.json(returnBooks);
     });
   }
   return { post, get };
